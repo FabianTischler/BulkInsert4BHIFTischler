471be18f6bf4bf0535c272438e656e358ca35cbe
FabianTischler <fabian.tischler.ft@gmail.com>
2020-02-07 08:24:58 +0100

    new test

---
+++ bulk_insert_demo/Persistence/RegularInserter.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using System.Linq;
+
+namespace BulkInsertDemo.Persistence
+{
+    public sealed class RegularInserter : StockUpdateHandlerBase
+    {
+        // this is a little rough but should suffice for the example
+        private readonly BulkContext _context;
+
+        public RegularInserter(BulkContext context)
+        {
+            this._context = context;
+        }
+
+        protected override async Task Persist(UpdatePackage package)
+        {
+            await using var transaction = await this._context.Database.BeginTransactionAsync();
+            foreach (var stockRow in GetRows(package))
+            {
+                int cnt = this._context.Stocks.Select(i => i.StoreNo).Where(i => i == stockRow.StoreNo).Count();
+                if (cnt >= 1)
+                {
+                    var entity = await this._context.Stocks.FindAsync(stockRow.StoreNo);
+                    entity.ProductCode = stockRow.ProductCode;
+                    entity.Stock = stockRow.Stock;
+                    entity.Timestamp = stockRow.Timestamp;
+                }
+                else
+                {
+                    await this._context.Stocks.AddAsync(stockRow);
+                }
+            }
+            await this._context.SaveChangesAsync();
+            await transaction.CommitAsync();
+        }
+    }
+}
